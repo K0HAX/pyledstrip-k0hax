e453710a89eac45214c258cdac47ef46fb05a3ee
Michael Englehorn <michael@englehorn.com>
2022-10-30 17:25:03 -0500

    Added TheaterChaseDual

--- src/pyledstrip_k0hax/TheaterChase.py
+++ src/pyledstrip_k0hax/TheaterChase.py
@@ -23,6 +23,28 @@ def TheaterChase(strip, color, wait_ms=50, iterations=10, stripState=None):
                 strip.setPixelColor(i + q, pixelColor)
     return stripState
 
+def TheaterChaseDual(strip, color1, color2, wait_ms=50, iterations=10, stripState=None):
+    """Movie theater light style chaser animation."""
+    logging.debug("TheaterChase type: {}".format(type(strip)))
+    logging.debug("Color1: {}".format(color1))
+    logging.debug("Color2: {}".format(color2))
+    if stripState == None:
+        stripState = StripState(strip)
+
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                stripState.SetPixel(i + q, color1)
+                strip.setPixelColor(i + q, color1)
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                #pixelColor = Color(0, 0, 0, 0)
+                pixelColor = color2
+                stripState.SetPixel(i + q, pixelColor)
+                strip.setPixelColor(i + q, pixelColor)
+    return stripState
+
 def TheaterChaseRainbow(strip, wait_ms=50, stripState=None):
     """Rainbow movie theater light style chaser animation."""
     if stripState == None:
